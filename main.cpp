a3931b67191d4a7bf61ce9cf78409045fcdd4f1a
JangVincent <vincentjangdev@gmail.com>
2025-10-07 00:41:39 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int Health{150};
+
+void TakeDamage() {
+  cout << " - TakeDamage function starting\n";
+  Health -= 50;
+  cout << " - TakeDamage function complete\n";
+}
+
+int main() {
+  cout << "Main function starting\n";
+  TakeDamage();
+  cout << "We are back in main\n";
+  TakeDamage();
+  cout << "Main function complete\n";
+}
